0447be0cea1273712a9989478f64fb2e5e578497
Nes280 <Nes280@users.noreply.github.com>
2016-09-25 20:54:22 +0200

    resolution du nf

--- core/Io.java
+++ core/Io.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Io {
 	public String RTID ="rtid=";
@@ -17,6 +19,9 @@ public class Io {
 	String relationship="";
 	String node="";
 	String bridge="";
+	
+	List<String> liste = new ArrayList<String>();
+	
 	
 	public void read(String path){
 		
@@ -30,17 +35,34 @@ public class Io {
 				if(dumpLine.startsWith(RID))
 				{
 					//write(dumpLine.replaceAll("rid=", "").replaceAll("n1=", "").replaceAll("n2=", "").replaceAll("t=", "").replaceAll("w=", "").replaceAll("\"", "") + "\n","bridge",true);
-					write(dumpLine.replaceAll("rid=|n1=|n2=|t=|w=|\"", "")+ "\n","bridge",true);
+					write(dumpLine.replaceAll("rid=|n1=|n2=|t=|w=|\"", "")+ "\n","bridge.nes",true);
 				}
 				else if(dumpLine.startsWith(EID))
 				{
 					//write(dumpLine.replaceAll("eid=", "").replaceAll("n=", "").replaceAll("t=", "").replaceAll("w=", "").replaceAll("\"", "") + "\n","node",true);
-					write(dumpLine.replaceAll("eid=|n=|t=|w=|\"", "")+ "\n","node",true);
+					dumpLine = dumpLine.replaceAll("eid=|n=|t=|w=|\"|nf=", "");
+					
+					// on split une ligne dans une liste
+					for (String retval: dumpLine.split("[|]")) {
+				         liste.add(retval);
+				         //System.out.println(liste.size() + " ---> "+ retval);
+				      }
+					//System.out.println(liste.size() );
+					if(liste.size() == 5) // si on a 5 elements c'est que on a un nf dans cette ligne
+					{
+						//System.out.println("set "+liste.get(4)+" to "+liste);
+						liste.set(1,liste.get(4)); // on remplace le code par sa valeur	
+						
+					}
+					
+					write(liste.get(0)+"|"+liste.get(1)+"|"+liste.get(2)+"|"+liste.get(3) + "\n","node.nes",true);
+					
+					liste.clear();
 				}
 				else if(dumpLine.startsWith(RTID))
 				{
 					//write(dumpLine.replaceAll("rtid=", "").replaceAll("name=", "").replaceAll("info=", "").replaceAll("\"", "")+ "\n","relationship",true);
-					write(dumpLine.replaceAll("rtid=|name=|info=|\"", "")+ "\n","relationship",true);
+					write(dumpLine.replaceAll("rtid=|name=|info=|\"", "")+ "\n","relationship.nes",true);
 
 				}
 				//if (!(dumpLine.startsWith("//")) && ((dumpLine.startsWith(RTID)) || (dumpLine.startsWith(RID)) || (dumpLine.startsWith(EID)))) write(dumpLine.replaceAll("rid=|n1=|n2=|t=|w=|\"|eid=|n=|rtid=|name=|info=", "")+ "\n","dump",true);
@@ -64,4 +86,5 @@ public class Io {
 			System.out.println(e.toString());
 		}
 	}
+
 }
--- core/Main.java
+++ core/Main.java
@@ -1,11 +1,16 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
+	
 
 	public static void main(String[] args) {
-		
+		logo();
 		// on demande à l'utilisateur où est le fichier
 		Scanner path = new Scanner(System.in);
 		System.out.println("Merci de donner le chemin absolu du fichier à traiter.");
@@ -18,9 +23,9 @@ public class Main {
 		//on lis le fichier
 		Io io = new Io();
 		
-		io.write(io.relationship, "relationship", false);
-		io.write(io.node, "node", false);
-		io.write(io.bridge, "bridge", false);
+		io.write(io.relationship, "relationship.nes", false);
+		io.write(io.node, "node.nes", false);
+		io.write(io.bridge, "bridge.nes", false);
 		
 		//io.write("", "dump", false);
 		io.read(rep);
@@ -29,6 +34,28 @@ public class Main {
 		//w.stop();
 		System.out.println("\n\nDone");
 
+	}
+	
+	public static void logo()
+	{
+		String logo="";
+		try {
+			
+			// Ouverture de dump
+			InputStream ipsDump = new FileInputStream("D:\\Users\\Niels\\Documents\\GitHub\\E-application\\core\\logo.txt");
+			InputStreamReader ipsrDump = new InputStreamReader(ipsDump);
+			BufferedReader brDump = new BufferedReader(ipsrDump);
+			
+			while ((logo = brDump.readLine()) != null) {
+				
+					System.out.println(logo);
+			}
+			
+			brDump.close();
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
 	}
 
 }
