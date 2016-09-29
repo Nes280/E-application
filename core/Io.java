3e18ac6b15e13408ac9e99c8e333bb15e568e2ae
Nes280 <n.benichou@gmail.com>
2016-09-29 17:03:07 +0200

    modif

--- core/Io.java
+++ core/Io.java
@@ -12,35 +12,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Io {
-    public String RTID = "rtid=";
-    public String EID = "eid=";
-    public String RID = "rid=";
+    public String RTID         = "rtid=";
+    public String EID          = "eid=";
+    public String RID          = "rid=";
 
-    String dumpLine;
-    String relationship = "";
-    String node = "";
-    String bridge = "";
+    String        dumpLine;
+    String        relationship = "";
+    String        node         = "";
+    String        bridge       = "";
 
-    List<String> liste = new ArrayList<String>();
-    PrintWriter fichierSortie3;
-    PrintWriter fichierSortie2;
-    PrintWriter fichierSortie1;
+    List<String>  liste        = new ArrayList<String>();
+    PrintWriter   fichierSortie3;
+    PrintWriter   fichierSortie2;
+    PrintWriter   fichierSortie1;
 
     public Io() throws IOException {
-    	//buffer du fichier relationship.csv
+        System.setProperty( "file.encoding", "UTF-8" );
+
+        // buffer du fichier relationship.csv
         FileWriter fw1 = new FileWriter( "relationship.csv", true );
         BufferedWriter bw1 = new BufferedWriter( fw1 );
         fichierSortie1 = new PrintWriter( bw1 );
+        fichierSortie1.print( "identifiant|nom1|nom2|desc\n" );
 
-        //buffer du fichier node.csv
+        // buffer du fichier node.csv
         FileWriter fw2 = new FileWriter( "node.csv", true );
         BufferedWriter bw2 = new BufferedWriter( fw2 );
         fichierSortie2 = new PrintWriter( bw2 );
+        fichierSortie2.print( "identifiant|mot|ty|poids\n" );
 
-        //buffer du fichier bridge.csv
+        // buffer du fichier bridge.csv
         FileWriter fw3 = new FileWriter( "bridge.csv", true );
         BufferedWriter bw3 = new BufferedWriter( fw3 );
         fichierSortie3 = new PrintWriter( bw3 );
+        fichierSortie3.print( "identifiant|mot1|mot2|rel|poids\n" );
     }
 
     public void readAndWrite( String path ) {
@@ -53,43 +58,50 @@ public class Io {
 
             while ( ( dumpLine = brDump.readLine() ) != null ) {
                 if ( dumpLine.startsWith( RID ) ) { // si on lis une arete
-                	
-                	// alors on supprime ce qui n'est pas utile est on ecrit dans le fichier bridge.csv
-                	fichierSortie3.print( dumpLine.replaceAll( "rid=|n1=|n2=|t=|w=|\"", "" ) + "\n");
-                    
-                } else if ( dumpLine.startsWith( EID ) ) {//si on lis un noeud
-                	
-                	//on supprime ce qui est inutile
+
+                    // alors on supprime ce qui n'est pas utile est on ecrit
+                    // dans le fichier bridge.csv
+                    fichierSortie3.print( dumpLine.replaceAll( "rid=|n1=|n2=|t=|w=|\"", "" ) + "\n" );
+
+                } else if ( dumpLine.startsWith( EID ) ) {// si on lis un noeud
+
+                    // on supprime ce qui est inutile
                     dumpLine = dumpLine.replaceAll( "eid=|n=|t=|w=|\"|nf=", "" );
 
                     // on split une ligne dans une liste
                     for ( String retval : dumpLine.split( "[|]" ) ) {
                         liste.add( retval );
                     }
-                    
+
                     if ( liste.size() == 5 ) // si on a 5 elements c'est que on
                                              // a un nf dans cette ligne
                     {
                         // System.out.println("set "+liste.get(4)+" to "+liste);
-                        liste.set( 1, liste.get( 4 ) ); // on remplace le code par sa valeur 
+                        liste.set( 1, liste.get( 4 ) ); // on remplace le code
+                                                        // par sa valeur
 
                     }
 
-                    // on recompose les element de la liste que l'on ecrit dans le fichier node.csv
-                    fichierSortie2.print( liste.get( 0 ) + "|" + liste.get( 1 ) + "|" + liste.get( 2 ) + "|" + liste.get( 3 ) + "\n");
+                    // on recompose les element de la liste que l'on ecrit dans
+                    // le fichier node.csv
+                    fichierSortie2.print( liste.get( 0 ) + "|" + liste.get( 1 ) + "|" + liste.get( 2 ) + "|"
+                            + liste.get( 3 ) + "\n" );
 
-                    //on efface le contenu de la liste pour eventuellement traiter une nouvelle ligne
+                    // on efface le contenu de la liste pour eventuellement
+                    // traiter une nouvelle ligne
                     liste.clear();
-                    
-                } else if ( dumpLine.startsWith( RTID ) ) {//si on lis une relation
-                	
-                	// alors on supprime ce qui n'est pas utile est on ecrit dans le fichier relationship.csv 
-                	fichierSortie1.print( dumpLine.replaceAll( "rtid=|name=|info=|\"", "" ) + "\n" );
+
+                } else if ( dumpLine.startsWith( RTID ) ) {// si on lis une
+                                                           // relation
+
+                    // alors on supprime ce qui n'est pas utile est on ecrit
+                    // dans le fichier relationship.csv
+                    fichierSortie1.print( dumpLine.replaceAll( "rtid=|name=|info=|\"|nom_etendu=", "" ) + "\n" );
 
                 }
             }
 
-            //le traitement est termine, donc on ferme tout ce qui a ete ouvert
+            // le traitement est termine, donc on ferme tout ce qui a ete ouvert
             brDump.close();
             fichierSortie3.close();
             fichierSortie2.close();
--- core/Main.java
+++ core/Main.java
@@ -10,42 +10,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main( String[] args ) throws IOException {
-        //Affichage du logo
-    	logo();
-        
+        System.setProperty( "file.encoding", "UTF-8" );
+        // Affichage du logo
+        logo();
+
         // on demande a l'utilisateur ou est le fichier
         Scanner path = new Scanner( System.in );
         System.out.println( "Merci de donner le chemin absolu du fichier a traiter." );
         String rep = path.nextLine();
-        
-        //preparation du calcule de la duree d'execution
+
+        // Scanner pathDef = new Scanner( System.in );
+        // System.out.println( "Merci de donner le chemin absolu du fichier de
+        // definition." );
+        // String repDef = path.nextLine();
+
+        // preparation du calcule de la duree d'execution
         long debut = System.currentTimeMillis();
-        
-        System.out.print("Execution du programme en cours...");
-        
+
+        System.out.print( "Execution du programme en cours..." );
+
         // on cree un objet Io puis on appel la methode read()
         // qui va lire et envoyer chaques lignes a write()
-        //pour l'ecrire dirrectement dans une fichier.
+        // pour l'ecrire dirrectement dans une fichier.
         Io io = new Io();
         io.readAndWrite( rep );
         path.close();
-        
-        System.out.print("  OK");
-        
-        //Affiche la duree d'execution en secondes
-        long temps =  (System.currentTimeMillis()-debut)/1000;
-        int minutes = (int) (temps/60);
-        int secondes = (int) (temps%60);
-        System.out.print( "\nFait en " + minutes + " minute(s) et " + secondes +" seconde(s).\n");
+
+        System.out.print( "  OK" );
+
+        // Affiche la duree d'execution en secondes
+        long temps = ( System.currentTimeMillis() - debut ) / 1000;
+        int minutes = (int) ( temps / 60 );
+        int secondes = (int) ( temps % 60 );
+        System.out.print( "\nFait en " + minutes + " minute(s) et " + secondes + " seconde(s).\n" );
     }
 
-    //Affiche un logo aucune utilitee particuliere .. 
+    // Affiche un logo aucune utilitee particuliere ..
     public static void logo() {
         String logo = "";
         try {
 
-            // Ouverture du fichier logo.txt 
-        	InputStream ipsDump = new FileInputStream("logo.txt");
+            // Ouverture du fichier logo.txt
+            InputStream ipsDump = new FileInputStream( "logo.txt" );
             InputStreamReader ipsrDump = new InputStreamReader( ipsDump );
             BufferedReader brDump = new BufferedReader( ipsrDump );
 
